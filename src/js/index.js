8af1e54f8b7cec2c8558139e07f289ef02a055c8
Laurent Bassin <laurent@bassin.info>
2018-04-13 16:19:18 +0200

    Display wireframe cube

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,23 @@
 // three.js
 import * as THREE from 'three';
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+camera.position.z = 1000;
+
+const geometry = new THREE.BoxGeometry(200, 200, 200);
+const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    wireframe: true
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+
+const scene = new THREE.Scene();
+scene.add(mesh);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+renderer.render(scene, camera);
